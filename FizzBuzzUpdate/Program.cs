eb9e757c886c0f9f29333cee665d1a8f0253517e
Michael Walker <walkerrepair255@yahoo.com>
2021-11-02 14:27:42 -0400

    Add project files.

---
+++ FizzBuzzUpdate/Program.cs
@@ -0,0 +1,19 @@
+﻿using WhatFun = System.Console;
+
+namespace FizzBuzzUpdate {
+    class Program {
+        static void Main(string[] args) { 
+            for (int i = 1; i < 100; i++) {
+                
+                var output = "";
+                
+                if (i % 3 == 0) { output += "Fizz"; }
+                if (i % 5 == 0) { output += "Buzz"; }
+                
+                if (output == "") { output += i; }
+                WhatFun.WriteLine(output);
+
+            }
+        }
+    }
+}
